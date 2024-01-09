b36ddee9775f04d3681aa08ae24ae9bf896ce2d9
Sahinur Islam <infosahinur@gmail.com>
2024-01-09 10:56:33 +0600

    FIX

--- Config.jsx
+++ Config.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseAxios = axios.create({
     
-    baseURL: "http://167.71.208.193/v1",
+    baseURL: "http://167.71.208.193:3009/v1",
     timeout: 10000,
     headers: {'X-Custom-Header': 'foobar'}
   });
